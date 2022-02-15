bcf229e4e456970a5d9967aaf4dbd8e98ea473c1
箕作 天志 <69094508+T-Kisaku@users.noreply.github.com>
2022-02-15 23:15:40 +0900

    Update

--- lib/data.tsx
+++ lib/data.tsx
@@ -4,10 +4,10 @@ const data = {
         name: 'SkyWill',
         description: () => (
             <>
-                <p>Hellow World!! I'm {data.profile.name}. I love tech!! </p>
+                <p>Hellow World!! I`&apos;`m {data.profile.name}. I love tech!! </p>
                 <p>Actually, I developed web service called pomofai.</p>
                 <p>If you use pomofai, you can manage task with pomodoro tequnique.</p>
-                <p><a href="https://pomofai.vercel.app" target="_blank">Pomofai URL</a></p>
+                <p><a href="https://pomofai.vercel.app" target="_blank" rel="noopener noreferrer">Pomofai URL</a></p>
             </>
         ),
         picture: '/my-icon.jpg',
